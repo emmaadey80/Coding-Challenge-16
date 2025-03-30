35637cbdbd3fa819547543eee9976f08f79ece06
Emma Adey <eadey@usf.edu>
2025-03-30 16:26:15 -0400

    Implement fetchProductsThen using .then() and .catch()

--- cc_16.js
+++ cc_16.js
@@ -1,1 +1,20 @@
-// Task 2: Fetch Products with .then()
+//  Task 2: Fetch() products with .then()
+function fetchProductsThen() {
+  fetch('https://www.course-api.com/javascript-store-products')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`HTTP ERROR!: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then(products => {
+      console.log('Product names:');
+      data.forEach(product => {
+        console.log(product.fields.name);
+      });
+    })
+    .catch(error => {
+      handleError(error);
+    });
+}
+
